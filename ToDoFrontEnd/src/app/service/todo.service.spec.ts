41ee67688f140f97c8672175de2a1fef3be8b2e2
Xu DU <XDu3@slb.com>
2022-11-07 16:50:52 +0800

    test(xdu3 qwan): add test for service.create()

--- ToDoFrontEnd/src/app/service/todo.service.spec.ts
+++ ToDoFrontEnd/src/app/service/todo.service.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { ToDoItem } from '../model/ToDoItem';
 import { TodoStoreService } from './todo-store.service';
 import { TodoService } from './todo.service';
 
@@ -6,8 +7,10 @@ describe('TodoService', () => {
 
   let service: TodoService;
   let todoStoreService: TodoStoreService;
+  let httpClient: any;
 
   beforeEach(() => {
+    httpClient =  jasmine.createSpyObj('HttpClient', ['post']);
     todoStoreService = new TodoStoreService();
     TestBed.configureTestingModule({});
     service = TestBed.inject(TodoService);
@@ -15,5 +18,14 @@ describe('TodoService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should create todoItem via mockHttp post', () => {
+    // given
+    const todoItem = new ToDoItem(1, 'title', 'description', true);
+    // when 
+    service.create(todoItem);
+    // then
+    expect(httpClient.post).toHaveBeenCalledWith('https://635fc244ca0fe3c21aa3d012.mockapi.io/api/todos', todoItem);
   });
 });
