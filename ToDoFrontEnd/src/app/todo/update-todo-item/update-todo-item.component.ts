98dd43ef3ce1b8cca0e7020dddc765d14e98ff0c
Xu DU <XDu3@slb.com>
2022-11-07 15:40:51 +0800

    refactor: get item id from route in update componment

--- ToDoFrontEnd/src/app/todo/update-todo-item/update-todo-item.component.ts
+++ ToDoFrontEnd/src/app/todo/update-todo-item/update-todo-item.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { TodoService } from '../../service/todo.service';
 import { ToDoItem } from '../../model/ToDoItem';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-update-todo-item',
@@ -10,14 +10,17 @@ import { Router } from '@angular/router';
 })
 export class UpdateTodoItemComponent implements OnInit {
 
-  get todoItem(): ToDoItem{
-    return this.todoService.currentUpdatingTodoItem();
-  }
+  todoItem: ToDoItem = new ToDoItem(0, '', '', false);
 
   constructor(public todoService: TodoService,
-              private router: Router) { }
+              private router: Router,
+              private activatedRoute: ActivatedRoute) { }
 
-  ngOnInit(): void { }
+  ngOnInit(): void { 
+    const id = this.activatedRoute.snapshot.paramMap.get('id');
+    console.log(id);
+    this.todoItem = this.todoService.findById(Number(id));
+  }
 
   update(): void {
     this.todoService.update(this.todoItem);
